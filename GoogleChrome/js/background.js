6c05addf9cb7c544a683978ec29f75e35d784e7c
northerneyes <gbuhanov@gmail.com>
2013-03-09 14:09:53 +0600

    Add angular, jquery library. Add popup.html. Add style.css

---
+++ GoogleChrome/js/background.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: George
+ * Date: 26.02.13
+ * Time: 22:03
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ GoogleChrome/js/popup.js
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: George
+ * Date: 26.02.13
+ * Time: 21:32
+ * To change this template use File | Settings | File Templates.
+ */
+function ColorSpaceCtrl($scope) {
+
+
+}
+
+//$(function() {
+//    // Render the template with the tabs data and insert
+//    // the rendered HTML under the "tabsList" element
+//  //  var tabs = chrome.extension.getBackgroundPage().tabs;
+////    $(".tabs-display" ).html(
+////        $( "#tabsTemplate" ).render(tabs)
+////    );
+//});
+
